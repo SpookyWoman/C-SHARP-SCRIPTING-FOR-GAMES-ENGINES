c2bff96ebe635c96ebaf5fd2bc5523eb82a39110
SpookyWoman <thedesolateether@gmail.com>
2024-02-27 21:30:53 +0000

    small edits

--- Assets/Scripts/Movepool.cs
+++ Assets/Scripts/Movepool.cs
@@ -1,31 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movepool : MonoBehaviour
 {
+    public int maxhealth = 100;
+    public int currenthealth = 100;
+    public image healthbar;
+
     public void Ember()
     {
-        //string type = "Fire"
-
+        string type = "Fire";
+        if (boolean Protect = false)
+        {
+            currenthealth = currenthealth - 15;
+            healthbar.fillAmount = (float)currenthealth / (float)maxhealth;
+        }
+        else
+        {
+            boolean Protect = false
+        }
     }
     public void Tackle()
     {
-        //string type = "Normal"
-
+        string type = "Normal";
+        if (boolean Protect = false)
+        {
+            currenthealth = currenthealth - 15;
+            healthbar.fillAmount = (float)currenthealth / (float)maxhealth;
+        }
 
     }
     public void WildCharge()
     {
-       // string type = "Electric"
-
+        string type = "Electric";
+        if (boolean Protect = false)
+        {
+            currenthealth = currenthealth - 15;
+            healthbar.fillAmount = (float)currenthealth / (float)maxhealth;
+        }
 
     }
 
     public void Protect()
     {
-        //string type = "Normal"
+        string type = "Normal";
+        boolean Protect = true
 
+    }
 
+    public void healthdamage()
+    {
+        healthbar.fillAmount = (float)currenthealth / (float)maxhealth;
     }
 }
